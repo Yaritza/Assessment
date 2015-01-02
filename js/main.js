4ce75ff45ae976911c048e4efdd1625666ae286c
Yaritza <saturn540@gmail.com>
2015-01-02 16:55:58 -0500

    fixed issues loading jQuery/$.  View site on localhost.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+'use strict';
 google.load("feeds", "1");
-google.load("jquery", "1.4.2");
 
 /*
  Use this idea to see if more items can be retrieved from the feed.
@@ -8,29 +8,48 @@ google.load("jquery", "1.4.2");
 
  function initialize() {
   var feed = new google.feeds.Feed("http://feeds2.feedburner.com/tedtalks_video/");
-      // feed = feed.setNumEntries(6);
-      feed.load(function(result) {
-        if (!result.error) {
-          var container = document.getElementById("feed");
-          for (var i = 0; i < result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            var div = document.createElement("div");
-            div.appendChild(document.createTextNode(entry.title));
-            container.appendChild(div);
-          }
-        }
-     // feed.setNumEntries(6);
-   });
-    };
+  /* Fix this to yield total count of entries on the Ted site. Use for setNumEntries(num) 
+  var num = feed.entries.length;
+  */
+  feed.setNumEntries(20);
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = document.getElementById("feed");
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode(entry.title));
+        container.appendChild(div);
+      }
+    }
+  });
+};
 
-    document.addEventListener("DOMContentLoaded", function(event) { 
-      google.setOnLoadCallback(initialize);
-      console.log("Js loaded");
+//Psuedo code-ish.  Look up if can use .content on entries.
+  // function detailedView() {
+  // var headlines = document.getElementById('feed').find('div');
+  // headlines.each(function(){
+  //   $(this).onclick(function(){
+  //           // alert("test");
+  //           // builds the slideOut div and toggles it for earch headline
+  //           var slideOutPanel = document.createElement('#div');
+  //           slideOutPanel.addClass('details');
+  //           $(this).appendChild(slideOutPanel.createTextNode(entry.conte));
+  //           slideOutPanel.slideToggle("fast");
+  //         }) 
+  //    }
+  // });
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  google.setOnLoadCallback(initialize);
+      // detailedView();
+      
     });
 
- 
+
 // Not sure why the "$" yields a undefined here.
 //  $(document).ready(function() {
 //  google.setOnLoadCallback(initialize);
+// console.log("Js loaded");
 //  });
- 
