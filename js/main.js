2b456e8e208266d2d74fb4c62519cd9fedabdca0
Yaritza <saturn540@gmail.com>
2015-01-02 19:34:24 -0500

    build out the html for entry tiles and entries' detailed content. Content details are not hidden yet.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,37 @@
 'use strict';
 google.load("feeds", "1");
 
-/*
- Use this idea to see if more items can be retrieved from the feed.
- google.load("feeds", "1",{"callback" : pageLoaded});
- */
+/* Use this idea to see if more items can be retrieved from the feed. google.load("feeds", "1",{"callback" : pageLoaded}); */
 
- function initialize() {
+function initialize() {
   var feed = new google.feeds.Feed("http://feeds2.feedburner.com/tedtalks_video/");
   /* Fix this to yield total count of entries on the Ted site. Use for setNumEntries(num) 
   var num = feed.entries.length;
   */
+ 
   feed.setNumEntries(20);
   feed.load(function(result) {
     if (!result.error) {
-      var container = document.getElementById("feed");
+      // var container = document.getElementById("feed");
+      var container = $('#feed');
       for (var i = 0; i < result.feed.entries.length; i++) {
         var entry = result.feed.entries[i];
-        var div = document.createElement("div");
-        div.appendChild(document.createTextNode(entry.title));
-        container.appendChild(div);
+        // building out the html for the feed titles and repective cotent details
+        var entryDiv = '<div class = "entry">' + '<div class="title">' + 
+         entry.title + '<div class="details">' + entry.content + '</div> </div>'
+
+        container.append(entryDiv)
       }
     }
   });
 };
 
-//Psuedo code-ish.  Look up if can use .content on entries.
-  // function detailedView() {
-  // var headlines = document.getElementById('feed').find('div');
-  // headlines.each(function(){
-  //   $(this).onclick(function(){
-  //           // alert("test");
-  //           // builds the slideOut div and toggles it for earch headline
-  //           var slideOutPanel = document.createElement('#div');
-  //           slideOutPanel.addClass('details');
-  //           $(this).appendChild(slideOutPanel.createTextNode(entry.conte));
-  //           slideOutPanel.slideToggle("fast");
-  //         }) 
-  //    }
-  // });
+  document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("Js is loading"); 
+    google.setOnLoadCallback(initialize);
+    //detailedView(); 
+  });
 
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-  google.setOnLoadCallback(initialize);
-      // detailedView();
-      
-    });
 
 
 // Not sure why the "$" yields a undefined here.
